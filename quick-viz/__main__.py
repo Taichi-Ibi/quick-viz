77ab36dd7a0b57f9070fcc85b0fac5a13c33795d
Taichi-Ibi <t.ibi@estyle-inc.jp>
2023-04-14 16:52:35 +0900

    [mod] コメント追加。ディレクトリ構成変更

--- quick-viz/__main__.py
+++ quick-viz/__main__.py
@@ -1,30 +1,42 @@
 import os
-import pandas as pd
-import pygwalker as pyg
 import subprocess
 
-print("csvファイルをドラッグして下さい")
+import pandas as pd
+import pygwalker as pyg
+
+# パスのリード
+print("csvファイルをドラッグして下さい。")
 path = input()[1:-1]
 
+# csvの判定
+assert ".csv" in path, "csvファイルではありません。"
+
+# csvをread
 try:
     df = pd.read_csv(path, encoding="utf-8")
 except:
     df = pd.read_csv(path, encoding="CP932")
+
+# pyg walkerをhtml化
 viz = pyg.to_html(df)
 
+# htmlのエンコーディング指定
 header = """
 <head>
 <meta charset="utf-8">
 </head>
 """
-
+# header情報を追加
 head = viz.split("\n")[:2]
 tail = viz.split("\n")[2:]
 viz = ("\n").join(head) + header + ("\n").join(tail)
 
-path = os.path.abspath("quick-viz/output/quick-viz.html")
+# 保存先を絶対パスで指定
+path = os.path.abspath("quick-viz.html")
 
+# htmlを出力
 with open(path, mode="w", encoding="utf-8") as f:
     f.write(viz)
 
+# htmlをopen
 subprocess.call(["open", path])
