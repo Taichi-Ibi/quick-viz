2e24009badfde01ec49a44c79c9fcd2be954a64e
Taichi-Ibi <t.ibi@estyle-inc.jp>
2023-04-14 11:22:14 +0900

    [mod] headerの追加

--- quick-viz/__main__.py
+++ quick-viz/__main__.py
@@ -3,5 +3,16 @@ import pygwalker as pyg
 
 df = pd.read_csv("quick-viz/sample_data/titanic_train.csv", encoding="utf-8")
 viz = pyg.to_html(df)
+
+header = """
+<head>
+<meta charset="utf-8">
+</head>
+"""
+
+head = viz.split("\n")[:2]
+tail = viz.split("\n")[2:]
+viz = ("\n").join(head) + header + ("\n").join(tail)
+
 with open("quick-viz/output/quick-viz.html", mode="w", encoding="utf-8") as f:
     f.write(viz)
