f01c76a11dd2d49f6d4c8945a42e1162c59a3ff4
Taichi-Ibi <t.ibi@estyle-inc.jp>
2023-04-14 18:50:24 +0900

    [mod] setup.py配置

--- qviz/__main__.py
+++ qviz/__main__.py
@@ -4,39 +4,45 @@ import subprocess
 import pandas as pd
 import pygwalker as pyg
 
-# パスのリード
-print("csvファイルをドラッグして下さい。")
-path = input().strip("'")
 
-# csvの判定
-assert ".csv" in path, "csvファイルではありません。"
+def main():
+    # パスのリード
+    print("csvファイルをドラッグして下さい。")
+    path = input().strip("'")
 
-# csvをread
-try:
-    df = pd.read_csv(path, encoding="utf-8")
-except:
-    df = pd.read_csv(path, encoding="cp932")
+    # csvの判定
+    assert ".csv" in path, "csvファイルではありません。"
 
-# pyg walkerをhtml化
-viz = pyg.to_html(df)
+    # csvをread
+    try:
+        df = pd.read_csv(path, encoding="utf-8")
+    except:
+        df = pd.read_csv(path, encoding="cp932")
 
-# htmlのエンコーディング指定
-header = """
-<head>
-<meta charset="utf-8">
-</head>
-"""
-# header情報を追加
-head = viz.split("\n")[:2]
-tail = viz.split("\n")[2:]
-viz = ("\n").join(head) + header + ("\n").join(tail)
+    # pyg walkerをhtml化
+    viz = pyg.to_html(df)
 
-# 保存先を絶対パスで指定
-path = os.path.abspath("quick-viz.html")
+    # htmlのエンコーディング指定
+    header = """
+    <head>
+    <meta charset="utf-8">
+    </head>
+    """
+    # header情報を追加
+    head = viz.split("\n")[:2]
+    tail = viz.split("\n")[2:]
+    viz = ("\n").join(head) + header + ("\n").join(tail)
 
-# htmlを出力
-with open(path, mode="w", encoding="utf-8") as f:
-    f.write(viz)
+    # 保存先を絶対パスで指定
+    path = os.path.abspath("quick-viz.html")
 
-# htmlをopen
-subprocess.call(["open", path])
+    # htmlを出力
+    with open(path, mode="w", encoding="utf-8") as f:
+        f.write(viz)
+
+    # htmlをopen
+    subprocess.call(["open", path])
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sample_command",
+    version="0.1.0",
+    author="cm-hirano-shigetoshi",
+    author_email="t.ibi@estyle-inc.jp",
+    description="Vizualize CSV quickly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Taichi-Ibi/qviz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={"console_scripts": ["qviz = qviz.qviz:main"]},
+    python_requires=">=3.7",
+)
