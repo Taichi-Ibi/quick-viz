d1ca90705cee1788830bca1badc27f4650d29699
Suzuki Yuji <yuji.suzuki@estyle-inc.jp>
2023-04-14 16:21:37 +0900

    [mod] inputの修正

--- quick-viz/__main__.py
+++ quick-viz/__main__.py
@@ -3,7 +3,10 @@ import pandas as pd
 import pygwalker as pyg
 import subprocess
 
-df = pd.read_csv("quick-viz/sample_data/titanic_train.csv", encoding="utf-8")
+print('csvファイルをドラッグして下さい')
+path = input()[1:-1]
+
+df = pd.read_csv(path, encoding="utf-8")
 viz = pyg.to_html(df)
 
 header = """
