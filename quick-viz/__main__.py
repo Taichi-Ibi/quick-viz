c2a962c089f359bc0ebb124e08c31a49fcec2a2e
Suzuki Yuji <yuji.suzuki@estyle-inc.jp>
2023-04-14 11:03:38 +0900

    [MOD] __main__.py [ADD] output

--- quick-viz/__main__.py
+++ quick-viz/__main__.py
@@ -2,4 +2,6 @@ import pandas as pd
 import pygwalker as pyg
 
 df = pd.read_csv("quick-viz/sample_data/titanic_train.csv", encoding="utf-8")
-print(pyg.to_html(df))
+viz = pyg.to_html(df)
+with open("quick-viz/output/quick-viz.html", mode="w", encoding="utf-8") as f:
+    f.write(viz)
