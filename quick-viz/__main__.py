646f314fbea96a35fde7c2b94c589d78521b1da5
Suzuki Yuji <yuji.suzuki@estyle-inc.jp>
2023-04-14 11:28:43 +0900

    [mod] path

--- quick-viz/__main__.py
+++ quick-viz/__main__.py
@@ -1,5 +1,7 @@
+import os
 import pandas as pd
 import pygwalker as pyg
+import webbrowser
 
 df = pd.read_csv("quick-viz/sample_data/titanic_train.csv", encoding="utf-8")
 viz = pyg.to_html(df)
@@ -14,5 +16,9 @@ head = viz.split("\n")[:2]
 tail = viz.split("\n")[2:]
 viz = ("\n").join(head) + header + ("\n").join(tail)
 
-with open("quick-viz/output/quick-viz.html", mode="w", encoding="utf-8") as f:
+path = os.path.abspath("quick-viz/output/quick-viz.html")
+
+with open(path, mode="w", encoding="utf-8") as f:
     f.write(viz)
+
+webbrowser.open(path)
