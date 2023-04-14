96e870edde1e72e212ba76cc316c06483ab0df8b
Taichi-Ibi <t.ibi@estyle-inc.jp>
2023-04-14 18:54:10 +0900

    [mod] ver up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sample_command",
-    version="0.1.0",
+    version="0.1.1",
     author="cm-hirano-shigetoshi",
     author_email="t.ibi@estyle-inc.jp",
     description="Vizualize CSV quickly",
